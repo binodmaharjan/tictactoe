f9bdbb920c5680ed960b2fa120d441846f82e8ff
binod <maharjan411@gmail.com>
2016-05-10 15:14:14 +0545

    ic_launcher changed

--- app/src/main/java/com/maharjan411/tictactoe/about/AboutActivity.java
+++ app/src/main/java/com/maharjan411/tictactoe/about/AboutActivity.java
@@ -2,6 +2,7 @@ package com.maharjan411.tictactoe.about;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.maharjan411.tictactoe.R;
 
@@ -12,5 +13,22 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+               this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
